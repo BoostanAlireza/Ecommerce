08dcb98db99e35e689aa305a8b097b100b02053c
Alireza Boostan <boostanalireza1999@gmail.com>
2025-01-07 20:58:13 +0330

    Add store, auth and debug urls

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+admin.site.site_header = 'Store'
+admin.site.index_title = 'Special Access'
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('store/', include('store.urls')),
+    path('store/', include('store.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
