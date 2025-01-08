b1281c850a22f43a2bea5676bebe9fe7d8a36007
Alireza Boostan <boostanalireza1999@gmail.com>
2025-01-08 20:17:06 +0330

    Register cart and cart_items

--- store/urls.py
+++ store/urls.py
@@ -1,4 +1,5 @@
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from . import views
 
 router = DefaultRouter()
@@ -6,5 +7,9 @@ router = DefaultRouter()
 router.register('products', views.ProductViewSet, basename='product')
 router.register('categories', views.CategoryViewSet, basename='category')
 router.register('customers', views.CustomerViewSet, basename='customer')
+router.register('carts', views.CartViewSet, basename='cart')
 
-urlpatterns = router.urls
+cart_items_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+cart_items_router.register('items', views.CartItemViewSet, basename='cart-items')
+
+urlpatterns = router.urls + cart_items_router.urls
